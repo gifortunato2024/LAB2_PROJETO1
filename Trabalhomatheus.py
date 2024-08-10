0bb6ddd63abb155221f5e7cb5c8c3c0aad7ce719
Giovanna <gifortunato2006@gmail.com>
2024-08-10 11:44:56 -0300

    terceiro passo

--- Trabalhomatheus.py
+++ Trabalhomatheus.py
@@ -1,4 +1,15 @@
+import random
+
 def cumprimento(texto):
     return f"Olá, {texto}"
 
 print(cumprimento("Giovanna Fortunato Pinto Santos"))
+
+def calcula_media_aleatoria():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    num3 = random.randint(1, 100)
+    media = (num1 + num2 + num3) / 3
+    return media
+
+print("Média de três números aleatórios:", calcula_media_aleatoria())
